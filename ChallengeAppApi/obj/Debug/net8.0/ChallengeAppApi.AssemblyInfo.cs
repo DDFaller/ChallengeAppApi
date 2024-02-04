b5af19b91c0fe1e7594805fa17d731c6bb7203db
Daniel Faller <danielmfaller@hotmail.com>
2024-02-04 16:01:48 -0300

    Deleting Views

--- ChallengeAppApi/obj/Debug/net8.0/ChallengeAppApi.AssemblyInfo.cs
+++ ChallengeAppApi/obj/Debug/net8.0/ChallengeAppApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengeAppApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a5edfc397cdd169ad010098904eeedf0780469")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengeAppApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengeAppApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
